f9fb75c24667954eca7949e4b72d4bc3608409a2
Andrés Ortega <elarcanista@gmail.com>
2018-02-20 16:23:21 -0500

    Correction in type names

--- NFA.hs
+++ NFA.hs
@@ -13,18 +13,18 @@ data NFA s a = NFA
   , delta  :: Delta s a
   } deriving Show
 
-initDFA :: Set.Set s -> DFA s a
-initDFA q0 =
-  DFA q0 Set.empty q0 Set.empty Map.empty
+initNFA :: Set.Set s -> NFA s a
+initNFA q0 =
+  NFA q0 Set.empty q0 Set.empty Map.empty
 
-trans :: (Ord a, Ord s) => (s, a, Set.Set s) -> DFA s a -> DFA s a
-trans (q1, a, q2) (DFA q0 f q s tf)
-  | q2 == Set.empty = (DFA q0 f q s tf)
-  | otherwise = DFA q0 f (Set.insert q1 (Set.union q2 q)) (Set.insert a s)
+trans :: (Ord a, Ord s) => (s, a, Set.Set s) -> NFA s a -> NFA s a
+trans (q1, a, q2) (NFA q0 f q s tf)
+  | q2 == Set.empty = (NFA q0 f q s tf)
+  | otherwise = NFA q0 f (Set.insert q1 (Set.union q2 q)) (Set.insert a s)
     (insert tf)
   where insert = Map.insertWith (Map.unionWith Set.union) q1
                  (Map.singleton a q2)
 
-accept :: (Ord a, Ord s) => Set.Set s -> DFA s a -> DFA s a
-accept q1 (DFA q0 f q s tf) =
-  DFA q0 (Set.union q1 f) (Set.union q1 q) s tf
+accept :: (Ord a, Ord s) => Set.Set s -> NFA s a -> NFA s a
+accept q1 (NFA q0 f q s tf) =
+  NFA q0 (Set.union q1 f) (Set.union q1 q) s tf
