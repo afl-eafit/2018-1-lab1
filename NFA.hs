1b34a501162e72c45cae7ea9b4571a7742b40d5f
Andrés Ortega <elarcanista@gmail.com>
2018-04-14 11:14:33 -0500

    Fixed several typos, list of importations is now on alphabetic order

--- NFA.hs
+++ NFA.hs
@@ -2,17 +2,17 @@
 Module      : NFA
 Description : Defines NFAs with multiple initial states
 
-To be used in CM0081 2018-1 lab 1. Definas NFAs and functions to create them
+To be used in CM0081 2018-1 lab 1. Defines NFAs and functions to create them
 -}
 module NFA where
 
 import qualified Data.Map as Map (Map, empty, singleton, insertWith, unionWith)
 import qualified Data.Set as Set (Set, empty, insert, union)
 
--- | Represents the transition funtion in a NFA
+-- | Represents the transition function in an NFA
 type Delta s a = Map.Map s (Map.Map a (Set.Set s))
 
--- | Represents a NFA as a 5-tuplei
+-- | Represents an NFA as a 5-tuple
 data NFA s a = NFA
   { start  :: Set.Set s -- ^ Set of initial states
   , final  :: Set.Set s -- ^ Set of accepting states
@@ -26,10 +26,10 @@ initNFA :: Set.Set s -- ^Initial states of the automata
         -> NFA s a   -- ^Automata with only initial states
 initNFA q0 = NFA q0 Set.empty q0 Set.empty Map.empty
 
--- | Adds transitions with of a state and symbol to a NFA
+-- | Adds transitions with of a state and symbol to an NFA
 trans :: (Ord a, Ord s)
       => (s, a, Set.Set s)
-      -- ^ Has the state where the transition starts, the symbol of the
+      -- ^ Triplet with he state where the transition starts, the symbol of the
       -- transition and the set of states in which the transition ends
       -> NFA s a
       -- ^ The automata to be modified
@@ -41,12 +41,12 @@ trans (q1, a, q2) nfa@(NFA q0 f q s tf)
   | otherwise       = NFA q0 f (Set.insert q1 (Set.union q2 q)) (Set.insert a s)
                         (insert q1 a q2 tf)
   where
-    -- | Inserts (q1, a, q2) into the delta function of a NFA
+    -- | Inserts (q1, a, q2) into the delta function of an NFA
     insert :: (Ord a, Ord s) => s -> a -> Set.Set s -> Delta s a -> Delta s a
     insert q1' a' q2' = Map.insertWith (Map.unionWith Set.union)
                      q1' (Map.singleton a' q2')
 
--- | Adds a set of acceptance states to a NFA
+-- | Adds a set of acceptance states to an NFA
 accept :: (Ord a, Ord s)
        => Set.Set s
        -- ^ Set of acceptance states to add
